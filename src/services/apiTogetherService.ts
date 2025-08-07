a20ca1868a6dd00ab4503b866995f7855121291a
erbert-gadelha <ebgr@cin.ufpe.br>
2025-08-07 03:18:11 -0300

    adicionado role maior especificação para o apiTogether.

--- src/services/apiTogetherService.ts
+++ src/services/apiTogetherService.ts
@@ -3,7 +3,7 @@ import Together from 'together-ai';
 const TOGETHER_API_KEY = "87654ae15d2fab07594678c07dd208e3216872d162495c69999a3598b0570879";
 const TOGETHER_API_URL = "https://api.together.xyz/v1/completions";
 //const MODEL_NAME = 'codellama/CodeLlama-7b-Instruct-hf';
-const MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2";
+const MODEL_NAME = "meta-llama/Llama-3.3-70B-Instruct-Turbo";
 
 
 export default class ApiTogetherService {
@@ -12,7 +12,15 @@ export default class ApiTogetherService {
         console.warn(`sending message:\n"${prompt}"`);
         const client = new Together({ apiKey: TOGETHER_API_KEY });
         const chatCompletion = await client.chat.completions.create({
-            messages: [{ role: 'user', content: prompt }],
+            messages: [{
+                    role: "system",
+                    content: "Você é um assistente de codificação que sempre retorna o arquivo .py completo, já corrigido, sem explicações. Mantenha todo o código original, exceto pelas correções necessárias."
+
+                }, {
+                    role: "user",
+                    content: prompt
+                }
+            ],
             model: MODEL_NAME,
         });
         console.warn('message received');
